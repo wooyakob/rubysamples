f92af95b074786852d02625d0728e7980534956d
wooyakob <jake_wood@mac.com>
2024-04-04 09:22:56 -0700

    output on a single line

--- c2f.rb
+++ c2f.rb
@@ -4,3 +4,9 @@ puts "The result is: "
 puts fahrenheit
 puts "."
 
+# the issue with this program is how the output displays when it runs
+
+# to display the result on a single line
+print "The result is: "
+print fahrenheit
+puts "."
