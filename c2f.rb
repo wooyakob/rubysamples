94ac9f51a9b626aa95b4ebfce9fbad29af6e70fc
wooyakob <jake_wood@mac.com>
2024-04-04 09:36:12 -0700

    allow user to enter celsius, convert to fahrenheit

--- c2f.rb
+++ c2f.rb
@@ -1,3 +1,4 @@
+=begin
 celsius = 100
 fahrenheit = (celsius * 9 / 5) + 32
 puts "The result is: "
@@ -10,3 +11,15 @@ puts "."
 print "The result is: "
 print fahrenheit
 puts "."
+=end
+
+# this program does not have much value, because it tells us over and over that 100 c equals 212 fh, more valuable would be specifying a c temperature and returning fh equivalent
+
+print "Hello. Please enter a Celsius value: "
+celsius = gets
+fahrenheit = (celsius.to_i * 9 / 5) + 32
+print ("The Fahrenheit equivalent is ")
+print fahrenheit
+puts "."
+
+
