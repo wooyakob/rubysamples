117e5fe6df672899afcc7a98c69f6526b6d262b5
wooyakob <jake_wood@mac.com>
2024-04-04 18:36:03 -0700

    update comments on objects

--- objects.rb
+++ objects.rb
@@ -1,1 +1,16 @@
- almost every line of ruby code revolves around an object
+=begin 
+almost every line of ruby code revolves around an object
+what you do with objects is send them messages
+the messages usually correspond to the names of methods you're asking the object to execute
+ruby objects are usally handled by variables that represent them
+bulk of design, logic and action revolves around objects
+when writing ruby programs, main activities: creating obects, endowing them with abilities and asking them to perform actions
+when you want something done:
+    a calculation
+    an output operation
+    a data comparison
+    you ask an object to do it
+
+
+        
+=end
