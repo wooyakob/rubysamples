765b5f0a08fc47faa3eb465ee937d4e32ff52b91
wooyakob <jake_wood@mac.com>
2024-04-06 13:22:18 -0700

    understanding objects

--- objects.rb
+++ objects.rb
@@ -10,7 +10,19 @@ when you want something done:
     an output operation
     a data comparison
     you ask an object to do it
-
-
-        
+    rather than ask whether a = b, you ask a (object) whether it considers itself equal to b
+    if you want to know whether a given student is taking a class from a given teacher,
+    you ask the student, are you a student of this teacher
 =end
+
+obj = Object.new
+
+# def is a keyword, obj is object to which method belongs, talk is method name
+def obj.talk
+    puts "I am an object."
+    puts "(Do you object?)"
+end
+
+# to ask obj to talk, use the message sending or method calling syntax, the object obj, responds to, the message talk
+obj.talk
+
